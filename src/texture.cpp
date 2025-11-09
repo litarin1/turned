8b48c4f47a0e9cda81fe1fad2d6df9a59a8e48a2
litarin1 <butyrinalek@yandex.ru>
2025-11-09 21:00:52 +0700

    great update - dynamic window size => viewport is now have the same dimensions as window - input: mouse input support in main.cpp - Mesh::construct_rect() -> ResourceManager::get_mesh_rect() - can specify pivot in ResourceManager::get_mesh_rect() to center, so Ship::pivot is not used anymore - `Game::debug_draw()`. behaviour for debug_draw() is custom for every kind of object - shaders: move shaders from main file - textures: store width and height; add simple static counter - FIX: ResourceManager was creating and not using objects

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,23 +1,27 @@
 #pragma once
 #include <GL/gl.h>
 #include <sys/types.h>
-#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 #include "log.cpp"
 
 class Texture {
     uint _id;
+    uint _w, _h;
 
 public:
+    // only get from ResourceManager
+    Texture(const Texture&) = delete;
+    Texture& operator=(const Texture&) = delete;
     Texture(const char* path) {
+        static uint count = 0;
         int w, h, nchannels;
         stbi_set_flip_vertically_on_load(true);
         u_char* data = stbi_load(path, &w, &h, &nchannels, 0);
         if (!data)
-            LERR("failed to load texture {}", path);
+            LERR("failed to load texture ({}) {}: {}", count, path, stbi_failure_reason());
         else {
-            LTRACE("{} {} {}", w, h, nchannels);
+            LTRACE("w{} h{} channels{} count{}", w, h, nchannels, count);
             glGenTextures(1, &_id);
             glBindTexture(GL_TEXTURE_2D, _id);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -26,10 +30,15 @@ public:
             // glGenerateMipmap(GL_TEXTURE_2D);
         }
         stbi_image_free(data);
+        count++;
+        _w = w;
+        _h = h;
     }
 
     inline void use(const u_char texture_unit) const {
         glActiveTexture(GL_TEXTURE0 + texture_unit);
         glBindTexture(GL_TEXTURE_2D, _id);
     }
+    inline uint w() const {return _w;}
+    inline uint h() const {return _h;}
 };
